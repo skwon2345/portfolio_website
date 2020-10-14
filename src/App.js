d943ffde25d46ca726772fbf1b7e35cf099cb4d5
skwon2345 <skwon2345@gmail.com>
2020-10-14 14:38:24 +0900

    test post'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList'
+import { uuid } from 'uuidv4'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  const API = process.env.REACT_APP_API;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handelAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return 
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuid(), name: name, complete: false}]
+    })
+    console.log(name)
+    todoNameRef.current.value = null
+  }
+
+  function handelClearTodos() {
+    
+    setName("sukkwon on");
+    setEmail("skwon2345@gmail.com");
+    setPassword("0123jdlfei");
+    const res = fetch(`${API}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    }).then(response => response.json())
+    .then(response => {
+      console.log('efe')
+    })
+
+    getUsers();
+  }
+
+  const getUsers = async () => {
+    const res = await fetch(`${API}/users`);
+    const data = await res.json();
+    console.log(data)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handelAddTodo}>Add Todo</button>
+      <button onClick={handelClearTodos}>Clear Completed</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+  )
 }
 
 export default App;
