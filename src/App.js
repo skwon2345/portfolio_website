2167c796c19a671d9a0c6bc61f391a48610e796c
skwon2345 <skwon2345@gmail.com>
2020-10-21 08:51:57 +0900

    new'

--- src/App.js
+++ src/App.js
@@ -1,81 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react';
-import TodoList from './TodoList'
-import { uuid } from 'uuidv4'
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
-  const API = process.env.REACT_APP_API;
-  // const API = process.env.REACT_APP_API_TEST;
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
-
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
-
-  function handelAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return 
-    setTodos(prevTodos => {
-      return [...prevTodos, {id: uuid(), name: name, complete: false}]
-    })
-    console.log(name)
-    todoNameRef.current.value = null
-  }
-
-  function handelClearTodos() {
-    
-    setName("asukkwdson on");
-    setEmail("eskwon2feu3fes45@gmail.com");
-    setPassword("0123jdlfei");
-    const res = fetch(`${API}/users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password,
-      }),
-    }).then(response => response.json())
-    .then(response => {
-      console.log('efe')
-    })
-
-    getUsers();
-  }
-
-  const getUsers = async () => {
-    const res = await fetch(`${API}/users`);
-    const data = await res.json();
-    console.log(data)
-  };
-  
   return (
-    <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handelAddTodo}>Add Todo</button>
-      <button onClick={handelClearTodos}>Clear Completed</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
-  )
+    <Router>
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
