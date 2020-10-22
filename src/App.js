380570f94ffacee85d5cbbb8117f8cc418905bf4
skwon2345 <skwon2345@gmail.com>
2020-10-22 23:45:05 +0900

    hello world

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './pages'
-import About from './pages/about'
-import SignUp from './pages/signup'
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/sign-up" exact component={SignUp} />
-      </Switch>
     </Router>
   );
 }
