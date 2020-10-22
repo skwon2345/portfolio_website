b39362781780011d008fd847237977738c80598e
skwon2345 <skwon2345@gmail.com>
2020-10-22 19:13:57 +0900

    &

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import './styles/index.css'
 const Home = () => {
     return (
         <>
-            <h1 className="title">Sukkwon <span id='on'> On</span><br/> Personal <br/> Page</h1>
+            <h1 className="title">Sukkwon <span id='on'> On</span> <br/> Personal Page</h1>
         </>
     )
 }
