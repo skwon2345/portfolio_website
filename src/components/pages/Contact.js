eeb4114394b2ac1e9a13025274fc5448311a3db3
skwon2345 <skwon2345@gmail.com>
2020-10-30 15:25:57 +0900

    'e'

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Contact() {
+    const apiAddress = process.env.REACT_APP_API_TEST
+
+    useEffect(() => {
+        // const headers = {
+        //     'accept': 'application/json'
+        // }
+        axios.get(apiAddress+'/api/data')
+        .then(res=>{
+            console.log(res)
+        })
+        .catch(err=>console.log(err))
+    },[]);
+
     return (
         <>
             <h1 className='contact'>Contact</h1>
