d4900772c713689c2f04a2da7dc110bd7e68cf0e
skwon2345 <skwon2345@gmail.com>
2020-11-01 10:25:57 +0900

    @

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,23 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import axios from 'axios';
+import './Contact.css'
 
 export default function Contact() {
     const apiAddress = process.env.REACT_APP_API
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     const headers = {
+    //         'accept': 'application/json'
+    //     }
+    //     axios.get(apiAddress+'/api/data', headers)
+    //     .then(res=>{
+    //         console.log(res)
+    //     })
+    //     .catch(err=>console.log(err))
+    // },[]);
+
+    const nameRef = useRef('')
+    const emailRef = useRef('')
+    const textRef = useRef('')
+
+    const emailSent = () => {
         const headers = {
             'accept': 'application/json'
         }
-        axios.get(apiAddress+'/api/data', headers)
+        const postData = {
+            name:'e'
+        }
+        axios.post(apiAddress+'/users', postData, headers)
         .then(res=>{
             console.log(res)
         })
         .catch(err=>console.log(err))
-    },[]);
+    }
 
     return (
         <>
-            <h1 className='contact'>Contact</h1>
+            <div className='wrapper_contact'>
+                <form className = "form" onSubmit={emailSent}>
+                    <h1>Contact form</h1>
+
+                    <label>Name</label>
+                    <input placeholder='name' ref={nameRef}/>
+
+                    <label>Email</label>
+                    <input placeholder='Email'/>
+
+                    <label>Message</label>
+                    <textarea placeholder='name'></textarea>
+
+                    <button type='sumbit'>Submit</button>
+                </form>
+            </div>
+            
         </>
     );
 }
