c43d0f8c61bddfd76212217518e3a9ef0198434c
skwon2345 <skwon2345@gmail.com>
2021-01-28 23:47:30 +0900

    'e'

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './About.css'
 
 export default function About() {
     return (
         <>
-            <h1 className='about'>About</h1>
+            <div className='title'>
+                <h1>
+                    A B O U T
+                </h1>
+            </div>
+            <img className='profile-img' src="/images/profile.jpg" alt="profile" />
+            <div className='_title'>Biography</div>
+            <div className='_body'>
+                <p>
+                    This program is implemented in Python, which is best-fit language for analyzing data. 
+                </p>
+            </div>
+            <div className='_title'>Projects</div>
+            <div className='_body'>
+                <p>
+                    This program is implemented in Python, which is best-fit language for analyzing data. 
+                </p>
+            </div>
         </>
     );
 }
