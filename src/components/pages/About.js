4c3e3b3bdc0636ab0c44ba34d6fe04bafae9945c
skwon2345 <skwon2345@gmail.com>
2021-01-31 14:52:51 +0900

    'e'

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,21 +19,22 @@ export default function About() {
                 {props => (
                     <div style={props}>
                         <img className='profile-img' src="/images/profile.jpg" alt="profile" />   
+                        <div className='_title'>Biography</div>
+                        <div className='_body'>
+                            <p>
+                                I am a university student from South Korea, major in Computing Science in Simon Fraser University, located in Vancouver.
+                                <br/>This is my first website, developed by using Python as backend, and React as frontend. I am always enthusiastic in learning a new area of computer science and I enjoy discussing algorithm with my friends.
+                                <br/>Love to code as a group, do not know how to give up when facing a huge problem.
+                                <br/>
+                                <br/>Currently working on stock analysis using python, and collecting stock data based on my stock algorithm to extends this automatic program to artificial intelligence program.
+
+                            </p>
+                        </div>
                     </div>
                 )}
             </Spring>
             
-            <div className='_title'>Biography</div>
-            <div className='_body'>
-                <p>
-                    I am a university student from South Korea, major in Computing Science in Simon Fraser University, located in Vancouver.
-                    <br/>This is my first website, developed by using Python as backend, and React as frontend. I am always enthusiastic in learning a new area of computer science and I enjoy discussing algorithm with my friends.
-                    <br/>Love to code as a group, do not know how to give up when facing a huge problem.
-                    <br/>
-                    <br/>Currently working on stock analysis using python, and collecting stock data based on my stock algorithm to extends this automatic program to artificial intelligence program.
 
-                </p>
-            </div>
         </>
     );
 }
