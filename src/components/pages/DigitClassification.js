5d994c3bc654277755901507f6f8f8ba96db2c77
skwon2345 <skwon2345@gmail.com>
2021-02-07 13:02:02 +0900

    'e'

--- src/components/pages/DigitClassification.js
+++ src/components/pages/DigitClassification.js
@@ -49,8 +49,7 @@ export default function DigitClassification() {
         setResult(null)
 
         const headers = {
-            "Content-Type": "text/plain",
-            "Access-Control-Allow-Origin": "*"
+            "Content-Type": "text/plain"
         }
 
         const fd = c
